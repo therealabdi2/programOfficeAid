fb0ee781ab62076e0ae9908eb69667ae8682e27d
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2022-03-14 19:54:23 +0500

    add form action and HTMX source package to base.html

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 from django import forms
+from django.urls import reverse_lazy
 
 from .models import Account
 
@@ -10,6 +11,7 @@ class RegisterForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper(self)
+        self.helper.form_action = reverse_lazy('accounts:register')
         self.helper.form_method = 'post'
         self.helper.add_input(Submit('submit', 'Register'))
 
