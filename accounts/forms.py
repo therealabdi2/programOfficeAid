f690bdc9a0fe97aad8ece7c17e58b282ed1acd09
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2022-03-15 00:00:24 +0500

    handle post request and store user in Database

--- accounts/forms.py
+++ accounts/forms.py
@@ -21,3 +21,10 @@ class RegisterForm(forms.ModelForm):
         widgets = {
             'password': forms.PasswordInput(),
         }
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 
@@ -7,4 +8,12 @@ from accounts.forms import RegisterForm
 class RegisterView(View):
     def get(self, request):
         context = {"form": RegisterForm()}
+        return render(request, 'accounts/register.html', context=context)
+
+    def post(self, request):
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return HttpResponse('HI')
+        context = {"form": form}
         return render(request, 'accounts/register.html', context=context)
