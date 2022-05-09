58319066f3ba58f895d1ad88c28c4f9ccc80dbce
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2022-05-09 14:15:18 +0500

    only get joining forms submitted by student

--- submissions/views.py
+++ submissions/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render
-
 # Create your views here.
-from django.views.generic import DetailView, ListView
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView
 
+from accounts.models import StudentProfile
 from submissions.models import Joining
 
 
 class JoiningFormView(ListView):
     model = Joining
     template_name = 'submissions/joining.html'
+    context_object_name = 'joiningforms'
 
+    def get_queryset(self):
+        profile = get_object_or_404(StudentProfile, student_id=self.request.user.id)
+        return self.model.objects.filter(student=profile)
