fe00b58e84b3c94a54c11fdf101aa7f1b412cfb0
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2022-05-13 00:32:06 +0500

    add joining form detail url and view where users can access their own forms and added dummy html template for testing purposes

--- submissions/urls.py
+++ submissions/urls.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.urls import path
 
-from submissions.views import JoiningFormView
+from submissions.views import JoiningFormView, JoiningDetailView
 
 app_name = 'submissions'
 
 urlpatterns = [
     path('joining_form/', login_required(JoiningFormView.as_view()), name='joining_form'),
+    path('joining_detail/<int:pk>/', login_required(JoiningDetailView.as_view()), name='joining_detail'),
 ]
--- submissions/views.py
+++ submissions/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 from django.shortcuts import get_object_or_404
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from accounts.models import StudentProfile
 from submissions.models import Joining
@@ -14,3 +14,14 @@ class JoiningFormView(ListView):
     def get_queryset(self):
         profile = get_object_or_404(StudentProfile, student_id=self.request.user.id)
         return self.model.objects.filter(student=profile).order_by('-id')[:3]
+
+
+class JoiningDetailView(DetailView):
+    model = Joining
+    template_name = 'submissions/joining_detail.html'
+    context_object_name = 'joiningform'
+
+    def get_queryset(self):
+        profile = get_object_or_404(StudentProfile, student_id=self.request.user.id)
+        return Joining.objects.filter(student=profile)
+
