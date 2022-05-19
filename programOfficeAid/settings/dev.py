a2bd914b89bd4460585e09ecde1d70174ddf11f7
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2022-05-19 17:29:42 +0500

    modify the web app settings for development

--- programOfficeAid/settings/dev.py
+++ programOfficeAid/settings/dev.py
@@ -8,3 +8,6 @@ ALLOWED_HOSTS = ['*']
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- programOfficeAid/urls.py
+++ programOfficeAid/urls.py
@@ -41,5 +41,7 @@ urlpatterns = [
     path(r'^ajax_select/', include(ajax_select_urls))
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
