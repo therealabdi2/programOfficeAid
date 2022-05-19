a2ecbc4bf348d6b64aee116d61c514445d4ba7ab
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2022-05-19 21:54:23 +0500

    added database connection

--- programOfficeAid/settings/base.py
+++ programOfficeAid/settings/base.py
@@ -98,8 +98,15 @@ if 'DATABASE_DEV' in os.environ:
             'PORT': os.environ.get('PORT'),
         }
     }
-else:
+elif 'DATABASE_PROD' in os.environ:
     DATABASES = {'default': dj_database_url.config()}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 AUTH_PASSWORD_VALIDATORS = [
