309172ffce4494b11b21de859259151e9a1749a6
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2022-05-20 16:51:05 +0500

    add collect static config

--- programOfficeAid/settings/base.py
+++ programOfficeAid/settings/base.py
@@ -38,8 +38,6 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
 ]
 
-
-
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
@@ -165,6 +163,27 @@ USE_TZ = True
 # static files
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+
+# AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+# AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+# AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+# AWS_DEFAULT_ACL = 'public-read'
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_S3_OBJECT_PARAMETERS = {
+#     'CacheControl': 'max-age=86400'
+# }
+# AWS_LOCATION = 'static'
+# AWS_QUERYSTRING_AUTH = False
+# AWS_HEADERS = {
+#     "Access-Control-Allow-Origin": '*',
+# }
+#
+# DEFAULT_FILE_STORAGE = 'storages.backend.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+# COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
+
 STATICFILES_DIRS = [
     'programOfficeAid/static',
 ]
